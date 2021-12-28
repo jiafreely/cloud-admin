47336f5929aa421bbb79016b333385d46f8e3dfe
xjh <778305094@qq.com>
2021-12-28 17:28:57 +0800

    添加elasticSearch和测试类

---
+++ service-consumers/src/main/java/com/cloud/service/config/elasticsearch/EsConfig.java
@@ -0,0 +1,11 @@
+package com.cloud.service.config.elasticsearch;
+
+/**
+ * @author xjh
+ * @version 1.0
+ * @ClassName: EsConfig
+ * @description: TODO
+ * @date 2021/12/28 9:21
+ */
+public class EsConfig {
+}
---
+++ service-consumers/src/test/java/com/cloud/service/elasticsearch/ElasticSearchTest.java
@@ -0,0 +1,11 @@
+package com.cloud.service.elasticsearch;
+
+/**
+ * @author xjh
+ * @version 1.0
+ * @ClassName: ElasticSearchTest
+ * @description: TODO
+ * @date 2021/12/28 10:07
+ */
+public class ElasticSearchTest {
+}
